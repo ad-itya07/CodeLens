96bdf483aca4d2556407be0675e09063085c734d
Aditya Jha <141293751+ad-itya07@users.noreply.github.com>
2026-04-08 11:49:12 +0530

    Added QueryRoutes and RepoRoutes

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import { router as QueryRoutes } from "./routes/queryRoutes.js"
+import { router as RepoRoutes } from "./routes/repoRoutes.js"
 
 dotenv.config();
 
@@ -7,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+
+app.use("/api/query", QueryRoutes);
+app.use("/api/repo", RepoRoutes);
 
 app.get("/health", (req, res) => {
     res.send("Server is running!")
