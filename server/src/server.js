eece758f5d37fb7b4b1f9df5aa2d185b0b73cb0c
Aditya Jha <141293751+ad-itya07@users.noreply.github.com>
2026-04-09 20:32:33 +0530

    Update .gitignore, package.json, and server.js to include dotenv configuration and new dependencies

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,7 @@
+import 'dotenv/config';
 import express from "express";
-import dotenv from "dotenv";
 import { router as QueryRoutes } from "./routes/queryRoutes.js"
 import { router as RepoRoutes } from "./routes/repoRoutes.js"
-
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
