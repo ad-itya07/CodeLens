e7f4cac1632cd79d7b3f4a39a8ca44976a7dab43
Aditya Jha <141293751+ad-itya07@users.noreply.github.com>
2026-04-09 20:35:57 +0530

    Refactor handleQuery for llm response handling and checking for vague queries

--- server/src/controllers/queryController.js
+++ server/src/controllers/queryController.js
@@ -1,5 +1,10 @@
 import { buildContext } from "../context/buildContext.js";
+import { generateResponse } from "../llm/generateResponse.js";
 import { scorer } from "../retrieval/scorer.js";
+
+function isVagueQuery(query) {
+    return query.split(" ").length <= 3;
+}
 
 export async function handleQuery(req, res) {
     try {
@@ -12,11 +17,25 @@ export async function handleQuery(req, res) {
         const { tokens, hasAction, results,  } = await scorer(query);
 
         const context = buildContext(query, tokens, results, hasAction);
+        if (!context.candidates || context.candidates.length === 0 && isVagueQuery(query)) {
+            return res.status(200).json({
+                explaination: "Query is too vague. Showing overall project overview.",
+                fallback: true,
+            });
+        }
+
+        const llmResponse = await generateResponse(context);
+
+        if (!llmResponse || !llmResponse.status) {
+            return res.json({
+                status: "error",
+                explanation: "LLM failed to return valid response"
+            });
+        }
+
+        if (llmResponse.status === "llm_error") return res.json(llmResponse);
         
-        return res.status(200).json({
-            status: "debugged_context",
-            context
-        })
+        return res.status(200).json(llmResponse);
     } catch (err) {
         console.error("Query Error: ", err);
         return res.status(500).json({ error: "Internal Server Error"});
