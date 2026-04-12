e60fd562b39c2e20545055afac2b137bedf869f0
Aditya Jha <141293751+ad-itya07@users.noreply.github.com>
2026-04-13 00:06:47 +0530

    - update server.js to use auth-routes

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from "express";
 import { router as QueryRoutes } from "./routes/queryRoutes.js"
 import { router as RepoRoutes } from "./routes/repoRoutes.js"
+import { router as AuthRoutes } from "./routes/authRoutes.js"
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use("/api/query", QueryRoutes);
 app.use("/api/repo", RepoRoutes);
+app.use("/api/auth", AuthRoutes);
 
 app.get("/health", (req, res) => {
     res.send("Server is running!")
